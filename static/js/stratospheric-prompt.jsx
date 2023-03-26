67d70812811d4eb5a501b1f43f5fc1db6ff84071
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-03-26 21:43:23 +0200

    Markdown prompt for ebook content

--- static/js/stratospheric-prompt.jsx
+++ static/js/stratospheric-prompt.jsx
@@ -1,4 +1,4 @@
-import { Markprompt } from "markprompt"
+import { Markprompt } from 'markprompt'
 
 function StratosphericPrompt() {
   return <Markprompt projectKey="2eVyM4MCCHsBOyF1mBPumCEwNOhe3IQp" />
