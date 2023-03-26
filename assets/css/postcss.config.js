eedce9de2bc62ea9598ebf220081e7a14d0c654e
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-03-27 00:05:03 +0200

    Markdown prompt for ebook content

---
+++ assets/css/postcss.config.js
@@ -0,0 +1,5 @@
+module.exports = {    
+    plugins: [
+        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+    ]
+}
---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ]
+}
